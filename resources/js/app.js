91a670625f9a55614e7bf43f5e7e7b8d565fbf2e
Lyudvig <lyudvig5541@gmail.com>
2024-10-15 01:55:01 +0400

    add change

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,11 @@
 import { createApp } from 'vue';
 import router from './router/router.js'; // Import the router
 import AppComponent from './App.vue';
-import Edit from './Pages/Edit.vue';
 
 // Create Vue app
 const app = createApp({
     components:{
         AppComponent,
-        Edit
-
     }
 });
 
